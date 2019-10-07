c7b4d9cb3705388e4b4c93870893256fbb00b7f9
Young Seok Seo <young7seo@gmail.com>
2019-10-07 15:55:16 -0400

    implemented comparator

--- lab4/part1.v
+++ lab4/part1.v
@@ -1,16 +1,15 @@
 module part1(SW, HEX1, HEX0);
-    // Display two values set by SW[7:4] and SW[3:0] to HEX1 and HEX0, respectively.
 
     input [7:0] SW;
     output [6:0] HEX1, HEX0;
 
-    4bit7seg U0 (SW[7:4], HEX1);
-    4bit7seg U1 (SW[3:0], HEX0);
+    disp_4bit7seg U0 (SW[7:4], HEX1);
+    disp_4bit7seg U1 (SW[3:0], HEX0);
 
-endmodule
+endmodule // Display two values set by SW[7:4] and SW[3:0] to HEX1 and HEX0, respectively.
 
-module 4bit7seg(X, M);
-    // Given a 4 bit input, display the corresponding value on a 7-segment display.
+
+module disp_4bit7seg(X, M);
 
     input [4:0] X;
     output [6:0] M;
@@ -23,4 +22,4 @@ module 4bit7seg(X, M);
     assign M[5] = (~X[2]&X[1]) | (X[2]&X[1]&X[0]) | (~X[3]&~X[2]&~X[1]&X[0]);
     assign M[6] = (~x[3]&~X[2]&~X[1]) | (X[2]&X[1]&X[0]);
 
-endmodule
+endmodule // Given a 4 bit input, display the corresponding value on a 7-segment display.
---
+++ lab4/part2.v
@@ -0,0 +1,5 @@
+`include "part1.v"
+
+
+
+
